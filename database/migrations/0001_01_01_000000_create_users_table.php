bf876db3e00365d3bce4989493bb63df31a81730
Taufik Hidayat <taufikhdyt@student.ub.ac.id>
2024-12-15 22:21:48 +0700

    fix: role

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar')->nullable();
-            $table->enum('role', ['user', 'admin']) -> default('user');
+            $table->enum('role', ['student', 'teacher', 'admin']) -> default('student');
             $table->rememberToken();
             $table->timestamps();
         });
